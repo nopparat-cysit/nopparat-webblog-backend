762bdc5e9dda09b8686a92a447108fce0c84b25d
nopparat <nopparat.cysit@gmail.com>
2026-02-09 02:28:12 +0700

    feat: Refactor CORS middleware configuration for improved clarity and maintainability

--- app.mjs
+++ app.mjs
@@ -6,18 +6,18 @@ import createPostValodation from "./middlewares/createPost.validation.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(cors()); // อนุญาตให้ทุกโดเมนเข้าถึงได้
+
 app.use(express.json());
-// app.use(
-//     cors({
-//       origin: [
-//         "http://localhost:5173", // Frontend local (Vite)
-//         "http://localhost:3000", // Frontend local (React แบบอื่น)
-//         "https://nopparat-webblog.vercel.app", // Frontend ที่ Deploy แล้ว
-//         // ✅ ให้เปลี่ยน https://your-frontend.vercel.app เป็น URL จริงของ Frontend ที่ deploy แล้ว
-//       ],
-//     })
-//   );
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173", // Frontend local (Vite)
+        "http://localhost:3000", // Frontend local (React แบบอื่น)
+        "https://nopparat-webblog.vercel.app", // Frontend ที่ Deploy แล้ว
+        // ✅ ให้เปลี่ยน https://your-frontend.vercel.app เป็น URL จริงของ Frontend ที่ deploy แล้ว
+      ],
+    })
+  );
 
 
 app.get("/health", async (req, res) => {
