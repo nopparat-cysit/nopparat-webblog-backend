898a39112fde987e14e23cdac70b69db9884a695
nopparat <nopparat.cysit@gmail.com>
2026-02-10 13:32:34 +0700

    feat: Update CORS middleware formatting and enhance server startup logging

--- app.mjs
+++ app.mjs
@@ -8,16 +8,17 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(
-    cors({
-      origin: [
-        "http://localhost:5173", // Frontend local (Vite)
-        "http://localhost:3000", // Frontend local (React แบบอื่น)
-        "https://nopparat-webblog.vercel.app", // Frontend ที่ Deploy แล้ว
-        // ✅ ให้เปลี่ยน https://your-frontend.vercel.app เป็น URL จริงของ Frontend ที่ deploy แล้ว
-      ],
-    })
-  );
-app.use("/posts", postRouter)
+  cors({
+    origin: [
+      "http://localhost:5173", // Frontend local (Vite)
+      "http://localhost:3000", // Frontend local (React แบบอื่น)
+      "https://nopparat-webblog.vercel.app", // Frontend ที่ Deploy แล้ว
+      // ✅ ให้เปลี่ยน https://your-frontend.vercel.app เป็น URL จริงของ Frontend ที่ deploy แล้ว
+    ],
+  }),
+);
+app.use("/posts", postRouter);
+
 // app.get("/health", async (req, res) => {
 //   try {
 //     const result = await pool.query(`SELECT * FROM posts`);
@@ -27,7 +28,14 @@ app.use("/posts", postRouter)
 //   }
 // });
 
+// app.listen(port, () => {
+//   console.log(`Server is running at ${port}`);
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
-});
+export default app;
+
+if (process.env.VERCEL !== "1") {
+  app.listen(port, () => {
+    console.log(`✅ Server running on http://localhost:${port}`);
+  });
+}
